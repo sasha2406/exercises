19af6e6ecc8a6c25b150d37c5f4397b43615d542
sasha2406 <sasha24065@gmail.com>
2020-06-14 11:41:26 +0300

    solution 1

---
+++ Homework/homework-1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InputConponent from './InputConponent';
+
+function App() {
+  return (
+    <div className="App">
+		 <p>Enter your text:</p>
+		 <InputConponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Homework/homework-1/src/InputConponent.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './InputConponent.css'
+
+const Input = () => {
+
+	const [inputValue, setInputValue] = useState('');
+	const [content, setContent] = useState('');
+
+	// const clickButton = useCallback(
+	// 	() => {
+	// 		console.log('work');
+	// 	}, []
+	// );
+
+	return (
+		<div className="input-block">
+			<input type="text" onChange={(e) => { setInputValue(e.target.value) }} />
+			<div>{inputValue}</div>
+			<div className="button-block">
+				<button onClick={() => { setContent(inputValue) }}>Push me</button>
+			</div>
+			<div className="text-block">
+				<p>Output text:</p>
+				<p>{content}</p>
+			</div>
+		</div>
+	)
+}
+
+export default Input;
